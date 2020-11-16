2511488680b00efc750444876425fc708a9886ad
GitJoeButt <jbkell2@yahoo.com>
2020-11-16 12:13:35 -0600

    initial debugging on table creation complete.

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,16 +1,17 @@
 CREATE TABLE departments(
 dept_no varchar(4) PRIMARY KEY,
-dept_name UNIQUE varchar
+dept_name varchar UNIQUE
 );
 
 CREATE TABLE titles(
 title_id varchar(4) PRIMARY KEY,
-title UNIQUE varchar
+title varchar UNIQUE
 );
 
 CREATE TABLE employees(
 emp_no integer PRIMARY KEY,
-FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+emp_title_id varchar(4) NOT NULL,
+FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 birth_date date,
 first_name varchar,
 last_name varchar,
@@ -19,7 +20,9 @@ hire_date date
 );
 
 CREATE TABLE salaries(
-FOREIGN KEY (emp_no) REFERENCES employees(emp_no) PRIMARY KEY,
+emp_no INTEGER NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+PRIMARY KEY (emp_no),
 salary INTEGER
 );
 
